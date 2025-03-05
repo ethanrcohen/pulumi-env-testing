11206543280fb60b43cc48c2b24f7aee6528e9e2
Ethan Cohen <ecohen56@gmail.com>
2025-03-05 09:24:34 -0500

    fixcompile

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const githubProvider = new github.Provider("github-provider", {
         owner: "ethanrcohen",
 	appAuth: {
 		installationId: "62112298",
-		pemFile: process.env.APP_KEY,
+		pemFile: process.env.APP_KEY!,
 		id: "1167133"
 	}
  });
